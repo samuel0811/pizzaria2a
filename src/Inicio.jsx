9e7f776dc613ab14f4e2eac97d3b405dfde03014
samuel0811 <samuelresendegarcia@gmail.com>
2024-06-10 08:30:36 -0300

    menu navbar

--- src/Inicio.jsx
+++ src/Inicio.jsx
@@ -2,7 +2,13 @@ const Inicio = () => {
 
     return (
         <>
-            <h2>Página de Início</h2>
+        <h2>Página de Início</h2>
+        <p>Aula do ReactJS</p>
+        <input type="text" id="texto" />
+        <input type="button" id="botao"
+            value="CLIQUE AQUI"
+            onClick={()=>alert('Olá Mundo')}
+            />
         </>
     )
 }
